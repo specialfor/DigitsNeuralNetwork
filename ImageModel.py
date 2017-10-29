9e4093dff15523154fd72e5d6568e30b16da01cc
Volodymyr Hryhoriev <v.hryhoriev@tecsynt.com>
2017-10-29 19:03:50 +0200

    Create 'ImageModel' class which contains pixels, filename and correct answer

---
+++ ImageModel.py
@@ -0,0 +1,10 @@
+class ImageModel:
+
+    filename = ''
+    pixels = []
+    answer = 0
+
+    def __init__(self, filename, pixels, answer):
+        self.filename = filename
+        self.pixels = pixels
+        self.answer = answer
